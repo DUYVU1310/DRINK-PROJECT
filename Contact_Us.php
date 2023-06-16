cf851acebd9190b40f25493e42157e7ec4a5d4e7
DUYVU1310 <duyvu1310@gmail.com>
2023-06-16 23:14:24 +0700

    Initial commit

---
+++ Contact_Us.php
@@ -0,0 +1,123 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="../demo/Contact_Us.css">
+
+    <!-- font text -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Tilt+Warp&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
+    <link href="https://fonts.googleapis.com/css2?family=Lato:ital@1&display=swap" rel="stylesheet">
+    <!-- cdn font-awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- cdn boostrap -->
+    <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/js/bootstrap.min.js" integrity="sha512-3dZ9wIrMMij8rOH7X3kLfXAzwtcHpuYpEgQg1OA4QAob1e81H8ntUQmQm3pBudqIoySO5j0tHN4ENzA6+n2r4w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css" integrity="sha512-t4GWSVZO1eC8BM339Xd7Uphw5s17a86tIZIj8qRxhnKub6WoyhnrxeCIMeAqBPgdZGlCcG2PrZjMc+Wr78+5Xg==" crossorigin="anonymous" referrerpolicy="no-referrer" /> -->
+
+    <!-- cdn jquery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</head>
+<body>
+    <header>
+        <div class="menu-wrap">
+            <div class="logo-wrap justify-center">
+                <div class="logo">
+                    <img src="img/logo.png">
+                </div>
+            </div>
+            <div class="nav-wrap justify-center">
+                <div class="nav">
+                    <ul class="navbar">
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                <span class="active">Home</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                <span>Product<i class="fa-solid fa-caret-down"></i></span>
+                            </a>
+                            <ul class="drop-down">
+                                <div class="drop-bar">
+                                    <li class="drop-item">
+                                        <a href="">Fruit juices</a>
+                                    </li>
+                                    <li class="drop-item">
+                                        <a href="">Vegetable Juices</a>
+                                    </li>
+                                    <li class="drop-item">
+                                        <a href="">Smoothies</a>
+                                    </li>
+                                    <li class="drop-item">
+                                        <a href="">Protein Shakes</a>
+                                    </li>
+                                    <li class="drop-item">
+                                        <a href="">Winter Menu</a>
+                                    </li>
+                                    <li class="drop-item">
+                                        <a href="">Chocolate Juices</a>
+                                    </li>
+                                    <li class="drop-item">
+                                        <a href="">Mock Tails</a>
+                                    </li>
+                                </div>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                <span>Category</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                <span>Feedback</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                <span>About Us</span>
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="">
+                                <span>Contact Us</span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="auth-wrap justify-center">
+                <div class="btn-auth ">
+                    <div class="cart btn-wrap">
+                        <a href=""><i class="fa-solid fa-cart-shopping fa-xl"></i></a>
+                    </div>
+                    <div class="btn-login btn-wrap">
+                        <a class="btn" href="" role="button">Login</a>
+                    </div>
+                    <div class="btn-signup btn-wrap">
+                        <a class="btn" href="" role="button">Sign Up</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+        
+
+
---
+++ Contact_Us2.php
@@ -0,0 +1,64 @@
+<?php require_once './Contact_Us.php' ?>
+<div class="section-wrap">
+    <section class="sec-1">
+        <div class="content-wrap1">
+            <div class="banner-1">
+                <div class="text-wrap">
+                    <div class="subtitle-wrap wrap">
+                            <P>Contact Us</P>
+                    </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="sec-2">
+        <div class="content-wrap2">
+            <div class="banner-1">
+                <div class="img-banner-left">
+                    <img class="img-left" src="img/pinapple-1.png">
+                </div>
+            </div>
+
+            <div class="banner-2">
+                <p class="text">  Purchase your juice at Shop Juice because we guarantee the use of fresh and delicious ingredients. We carefully select fruits and vegetables to ensure the best flavor and quality for your juice. You will experience the refreshing and nourishing taste right from the first sip.</p>
+            </div>
+
+            <div class="banner-3">
+                <div class="banner-wrap">
+                    <div class="img-banner-right">
+                        <img class="img-right" src="img/blueberry-1.png">
+                    </div>
+                </div>
+                    
+            </div>
+        </div>
+    </section>
+
+    <section class="sec-3">
+        <div class="content-wrap3">
+            <div class="wrap3">
+                <div class="iframe1">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.924278786091!2d105.81645427605473!3d21.03571558753807!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0d127a01e7%3A0xab069cd4eaa76ff2!2zMjg1IFAuIMSQ4buZaSBD4bqlbiwgVsSpbmggUGjDuiwgQmEgxJDDrG5oLCBIw6AgTuG7mWkgMTAwMDAwLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1686539310144!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+                <div class="input">
+                    <div>
+                        <p>NAME</p>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <p>NAME</p>
+                        <input type="text">
+                    </div>
+                    <div>
+                        <p>NAME</p>
+                        <input type="text-area">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
+
+
+</body>
+<?php require_once './Contact_Us3.php' ?>
